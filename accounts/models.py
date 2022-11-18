3404f7f79ca10d702fdd29467a8679de4bb9ded2
Karl <X00189280@mytudublin.ie>
2022-11-18 12:30:17 +0000

    lab 9 part 2 commit

--- accounts/models.py
+++ accounts/models.py
@@ -13,8 +13,8 @@ class Profile(models.Model):
         null=True,
         on_delete=models.CASCADE,
         )
-    date_of_birth = models.DateField(blank=False, null=False)
-    fav_author = models.CharField(max_length=255)
+    date_of_birth = models.DateField(blank=True, null=True)
+    fav_author = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return str(self.user)
